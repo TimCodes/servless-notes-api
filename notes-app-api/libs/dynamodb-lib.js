ad3fe3c102f2a3f713a598cce0e2b4b8f5ee2392
timcodes <hardtimo@isu.edu>
2020-08-29 18:09:13 -0600

    got basic crud functionality workin

---
+++ notes-app-api/libs/dynamodb-lib.js
@@ -0,0 +1,16 @@
+import AWS from "aws-sdk";
+// only needed to test in local env
+const client = new AWS.DynamoDB.DocumentClient({
+  region: "us-west-2",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+AWS.config.update({ region: "us-west-2" });
+export default {
+  get: (params) => client.get(params).promise(),
+  put: (params) => client.put(params).promise(),
+  query: (params) => client.query(params).promise(),
+  update: (params) => client.update(params).promise(),
+  delete: (params) => client.delete(params).promise(),
+};
